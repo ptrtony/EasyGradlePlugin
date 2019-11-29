0eab07728f5ec49d02a536fa6a7013189d862caa
ptrtony <1029651482@qq.com>
2019-11-29 19:48:16 +0800

    使用简单案例阐述gradle插件的用法

--- buildSrc/src/main/groovy/com/zhaofan/plugin/TestPlugin.groovy
+++ buildSrc/src/main/groovy/com/zhaofan/plugin/TestPlugin.groovy
@@ -1,14 +1,18 @@
 package com.zhaofan.plugin
 
+import com.android.build.gradle.BaseExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-public class TestPlugin implements Plugin<Project> {
+class TestPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
         def extension = project.extensions.create('testCodePlugin',TestExtension)
-        target.afterEvaluate {
+        project.afterEvaluate {
             println("$extension.name")
         }
+        def testTransform = new TestTransform()
+        def testBaseExtension = project.extensions.getByType(BaseExtension)
+        testBaseExtension.registerTransform(testTransform)
     }
 }
